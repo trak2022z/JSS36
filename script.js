ff9ff1967ed727d3cc61ca487172c3904c5e07e4
TomKrok1 <91782249+trak2022z@users.noreply.github.com>
2022-03-26 16:25:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+(function() {
+    window.addEventListener('load', init);
+
+    function init() {
+let kitten = {
+name: "Orange", // string
+breedName: "Domestic Short hair", // string
+meow: function() {
+console.log(this.name + " says woof!"); // function
+},
+age: 3, // number
+toys: ["mouse", "fuzzy hat", "shoe string"] // array
+};
+console.log(kitten.toys[1]); // fuzzy hat
+kitten.meow(); // Orange says meow!
+console.log(kitten["breedName"]); // Domestic Short hair
+console.log(kitten.breedName); // Domestic Short hair   
+    }
+})();
+
